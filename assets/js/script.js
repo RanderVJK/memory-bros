87f5cec258f0758bb9000546a0b2f2fa705282eb
Vanderlei Kempf <dev.vjk@gmail.com>
2021-10-24 19:32:16 -0300

    Create function shuffle cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ let firstCard, secondCard;
 let lockBoard = false;
 
 function flipCard() {
-  if(!lockBoard) return;
+  if(lockBoard) return;
   if(this === firstCard) return;
 
   this.classList.add('flip');
@@ -31,6 +31,8 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
 }
 
 function unflipCards() {
@@ -48,6 +50,13 @@ function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
 
 cards.forEach((card) => {
   card.addEventListener('click', flipCard);
